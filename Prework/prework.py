db432c5ea781d2b734e0f860a9348b18795431ac
Sbengineermanager <sb.virtualasst@gmail.com>
2023-12-20 11:23:24 -0500

    first commit

---
+++ Prework/prework.py
@@ -0,0 +1,25 @@
+# Question 1 Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+# def hello_name(user_name):
+
+user_name = "hello_USERNAME!"
+print (user_name)
+
+USERNAME = "hello_Safiya!"
+print (USERNAME)
+
+USERNAME = "hello_Safiya!"
+print (USERNAME.upper())
+
+# Question 2 Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+# def first_odds():
+
+first_odds = list(range(1,100,2))
+print(first_odds)
+
+# Question 3 Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
+#def max_num_in_list(a_list):
+
+a_list = [2,3,4,5,6,1]
+print(max(a_list))  
+
+# Question 4 Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
